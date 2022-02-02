779f8a103f2738493365aaded08f1626215f7921
Esanic <Tofflund@hotmail.com>
2022-02-02 16:04:51 +0100

    Lade till kommentar om problemet.

--- GruppSnake/Tail.cs
+++ GruppSnake/Tail.cs
@@ -15,9 +15,10 @@ namespace GruppSnake
             this.appearance = appearance;
             this.world = world;
             this.spelare = spelare;
-
         }
 
+        //Svansen är hårdkodad att börja efter world.gameObjects[1].
+        //Har provat att använda for-loop men då blir svansen centrerad utan att följa efter Ormen.
         public override void Update()
         {
                 if (spelare.direction == Direction.Left)
@@ -36,27 +37,7 @@ namespace GruppSnake
                 {
                     position = new Position(world.gameObjects[1].position.x, world.gameObjects[1].position.y - 1);
                 }
-            //else
-            //{
-            //    for(int i = 3; i < world.gameObjects.Count; i++) { 
-            //    if (spelare.direction == Direction.Left)
-            //    {
-            //        position = new Position(world.gameObjects[i].position.x + 1, world.gameObjects[i].position.y);
-            //    }
-            //    if (spelare.direction == Direction.Right)
-            //    {
-            //        position = new Position(world.gameObjects[i].position.x - 1, world.gameObjects[i].position.y);
-            //    }
-            //    if (spelare.direction == Direction.Up)
-            //    {
-            //        position = new Position(world.gameObjects[i].position.x, world.gameObjects[i].position.y + 1);
-            //    }
-            //    if (spelare.direction == Direction.Down)
-            //    {
-            //        position = new Position(world.gameObjects[i].position.x, world.gameObjects[i].position.y - 1);
-            //    }
-            //    }
-            //}
+
 
 
         }
